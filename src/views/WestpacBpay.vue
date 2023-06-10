38ed4d5cfaf0b0e4ad99fa3b431d7b059e33fc6d
Adam Austin <adamjaustin84@gmail.com>
2023-06-10 16:04:56 +1000

    wip

--- src/views/WestpacBpay.vue
+++ src/views/WestpacBpay.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>
-    <p v-bind:key="row" v-for="row in rows">
-     Westpack!
-
-      {{row}}
-      <button v-key="row" class="button download-button">
-        Download
-    </button>
+  <div class="card">
+    <div class="columns">
+      <label for="ccn" class="column">CCN</label>
+      <label for="amount" class="column">Amount</label>
+    </div>
+    <p v-bind:key="index" v-for="(row, index) in rows" class="columns">
+      <input
+        class="input is-small is-pulled-right"
+        type="text"
+        id="ccn"
+        name="ccn"
+        aria-labelledby="#ccn"
+        placeholder="CCN"
+        :value="row.ccn"
+      />
+      <input
+        class="input is-small is-pulled-right"
+        type="text"
+        id="amount"
+        name="amount"
+        aria-labelledby="#amount"
+        placeholder="Amount"
+        :value="row.amount"
+      />
+      <button class="button remove row-button" @click='deleteRow'>
+        Remove Row
+      </button>
+      <button class="button add row-button" @click='addNewRow'>
+        Add Row
+      </button>
     </p>
   </div>
 </template>
@@ -15,35 +37,50 @@ export default {
   name: 'WestpackBpay',
   data() {
     return {
-      rows: [1, 23, 3],
+      rows: [{ ccn: 123, name: 'Adam' }, { ccn: 346, name: 'Austing' }],
     };
+  },
+  methods: {
+    addNewRow() {
+      const newRow = { ccn: 678, name: 'new row' };
+      this.rows = { ...this.rows, newRow };
+    },
+    deleteRow() {
+      if (this.rows.lenth <= 1) {
+        return;
+      }
+      this.rows.pop();
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
-$accentcolor: #fff;
-$lightcolor: #fff;
-$darkcolor: #444;
-
-.options {
-  position: relative;
+.card {
+  margin: 40px 40px;
+  padding: 30px;
 }
-
-.download-button {
-  font-family: 'Fira Sans', sans-serif;;
-  background-color: #31BFA0;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  border-radius: 0 0 8px 8px;
+.row-button {
+  &.add {
+    background-color: #31BFA0;
+    &:hover {
+      background-color: #37c0a3;
+      border:2px solid #31BFA0;
+    }
+  }
+  &.remove {
+    background-color: #dc146c;
+    &:hover {
+      background-color: #dc146c;
+      border:2px solid #dc146c;
+    }
+  }
+  font-family: 'Fira Sans', sans-serif;
+  border-radius: 8px 8px 8px 8px;
   transition: all ease-in-out 0.2s;
   box-shadow: 1px 1px 12px 2px rgba(0,0,0,0.2);
   &:hover {
     transition: all ease-in-out 0.2s;
     box-shadow: 2px 2px 10px 3px rgba(0,0,0,0.3);
-    background-color: #37c0a3;
-    border:2px solid #31BFA0;
     font-size: 1.1em;
   }
 }
